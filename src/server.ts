1738e90769b2f913ad6e8312c53bf4e98201ef3e
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-04-18 23:52:08 +0800

    search for latest file in user directory

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import path from 'path';
 import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
-import shelljs from 'shelljs'
+import shelljs from 'shelljs';
+import { glob } from 'glob';
+import fs from 'fs';
+import { log } from 'console';
 
 const swapDir = process.env.SWAP_DIR;
 const homeDir = process.env.HOME_DIR;
+
+if (!homeDir || !swapDir) throw new Error('SWAP_DIR and HOME_DIR must be specified');
 
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
@@ -34,6 +39,7 @@ bot.use(async (ctx, next) => {
 
 bot.on(message('text'), async (ctx) => {
   const message = ctx.message;
+  const userId = message.from.id;
 
   console.log('message', message);
 
@@ -43,9 +49,22 @@ bot.on(message('text'), async (ctx) => {
     return;
   }
 
-  const command = prepareYTDLCommand(message.from.id, message.text);
+  const command = prepareYTDLCommand(userId, message.text);
 
   shelljs.exec(command);
+
+
+  const pattern = path.join(homeDir, String(userId), '*');
+  console.log('pattern:', pattern);
+  const files = glob.sync(pattern, { windowsPathsNoEscape: true });
+  console.log('files:', files);
+
+  const newestFile = files
+    .map(name => ({ name, ctime: fs.statSync(name).ctime }))
+    .sort((a: { ctime: Date }, b: { ctime: Date }): number => b.ctime.getTime() - a.ctime.getTime())[0].name
+
+  log('newestFile:', newestFile);
+
 
   ctx.reply('short downloaded')
 });
@@ -56,7 +75,6 @@ bot.launch();
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-const r = /s/
 function isValidShortURL(url: string) {
   try {
     const l = new URL(url);
